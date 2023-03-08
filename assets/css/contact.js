87d18eecb20d79c394cccf53c309535f14b3a045
Fernando Almeida <nando.lalmeida@gmail.com>
2023-03-07 23:39:09 -0500

    finished contact page

--- assets/css/contact.js
+++ assets/css/contact.js
@@ -1,85 +1,104 @@
-$(document).ready(function(){
-    
-    (function($) {
-        "use strict";
+document.addEventListener("DOMContentLoaded", function() {
+    "use strict";
 
-    
-    jQuery.validator.addMethod('answercheck', function (value, element) {
-        return this.optional(element) || /^\bcat\b$/.test(value)
-    }, "type the correct answer -_-");
+    // validate contact form
+    const contactForm = document.querySelector("#contactForm");
+    const nameInput = contactForm.querySelector("#name");
+    const subjectInput = contactForm.querySelector("#subject");
+    const phoneInput = contactForm.querySelector("#phone");
+    const emailInput = contactForm.querySelector("#email");
+    const messageInput = contactForm.querySelector("#message");
 
-    // validate contactForm form
-    $(function() {
-        $('#contactForm').validate({
-            rules: {
-                name: {
-                    required: true,
-                    minlength: 2
-                },
-                subject: {
-                    required: true,
-                    minlength: 5
-                },
-                number: {
-                    required: true,
-                    minlength: 5
-                },
-                email: {
-                    required: true,
-                    email: true
-                },
-                message: {
-                    required: true,
-                    minlength: 20
-                }
+    const nameError = contactForm.querySelector("#nameError");
+    const subjectError = contactForm.querySelector("#subjectError");
+    const phoneError = contactForm.querySelector("#phoneError");
+    const emailError = contactForm.querySelector("#emailError");
+    const messageError = contactForm.querySelector("#messageError");
+
+    let isValid = false;
+
+    $(contactForm).validate({
+        rules: {
+            name: {
+                required: true,
+                minlength: 2
             },
-            messages: {
-                name: {
-                    required: "Name is required!",
-                    minlength: "A name should contain at least 2 characters"
-                },
-                subject: {
-                    required: "Subject is required!",
-                    minlength: "A subject must contain at least 5 characters"
-                },
-                number: {
-                    required: "Number is required!",
-                    minlength: "A number must contain at least 5 characters"
-                },
-                email: {
-                    required: "Email is required!"
-                },
-                message: {
-                    required: "Message is required!",
-                    // minlength: "thats all? really?"
-                }
+            subject: {
+                required: true,
+                minlength: 5
             },
-            submitHandler: function(form) {
-                $(form).ajaxSubmit({
-                    type:"POST",
-                    data: $(form).serialize(),
-                    // url:"contact_process.php",
-                    success: function() {
-                        $('#contactForm :input').attr('disabled', 'disabled');
-                        $('#contactForm').fadeTo( "slow", 1, function() {
-                            $(this).find(':input').attr('disabled', 'disabled');
-                            $(this).find('label').css('cursor','default');
-                            $('#success').fadeIn()
-                            $('.modal').modal('hide');
-		                	$('#success').modal('show');
-                        })
-                    },
-                    error: function() {
-                        $('#contactForm').fadeTo( "slow", 1, function() {
-                            $('#error').fadeIn()
-                            $('.modal').modal('hide');
-		                	$('#error').modal('show');
-                        })
-                    }
-                })
+            phone: {
+                required: true,
+                minlength: 5
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            message: {
+                required: true,
+                minlength: 20
             }
-        })
-    })
-        
- })(jQuery)
-})
+        },
+        messages: {
+            name: {
+                required: "Name is required!",
+                minlength: "A name should contain at least 2 characters"
+            },
+            subject: {
+                required: "Subject is required!",
+                minlength: "A subject must contain at least 5 characters"
+            },
+            phone: {
+                required: "Phone is required!",
+                minlength: "A phone must contain at least 5 characters"
+            },
+            email: {
+                required: "Email is required!"
+            },
+            message: {
+                required: "Message is required!",
+                // minlength: "thats all? really?"
+            }
+        },
+        errorPlacement: function(error, element) {
+            // Add red error message under the input field
+            error.css({color: 'red', marginLeft: '10px'});
+            error.insertAfter(element);
+        },
+        submitHandler: function(form) {
+            // send contact form data using EmailJS
+            const serviceId = "service_nb2pooc";
+            const templateId = "template_whkgyhi";
+            emailjs.send(serviceId, templateId, {
+                name: nameInput.value,
+                email: emailInput.value,
+                phone: phoneInput.value,
+                subject: subjectInput.value,
+                message: messageInput.value
+            }).then((res) => {
+                // display success message
+                const successModal = document.getElementById("successModal");
+                successModal.style.display = "block";
+                contactForm.reset();
+            }, function(err) {
+                // display error message
+                const errorModal = document.getElementById("errorModal");
+                errorModal.style.display = "block";
+            });
+        }
+    });
+    
+    // Get the modal element 
+    const modal = document.getElementById("successModal");
+
+    // Get the close button
+    const closeBtn = document.getElementById("close");
+
+    // When the user clicks the close button or anywhere outside the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal || event.target == closeBtn) {
+            modal.style.display = 'none';
+        }
+    };
+});
